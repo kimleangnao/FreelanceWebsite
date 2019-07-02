29b90a2d0b6195e446af7db5030a2dd00ad4495c
Leang <kimleangn@gmail.com>
2019-07-01 18:52:56 -0700

    Fixing some time, when double click on nav, it will lead to white space
    Fixed white space bug

--- js/index.js
+++ js/index.js
@@ -2,16 +2,19 @@
 
 	$('#contact').on('click', function(){
 		$('#first').animate({
-			marginLeft: '+=120em'
+			marginLeft: '120em'
 		}, 1000, function(){
 			$('#first').hide();
-			$('#second').css('marginLeft', '-120em')
+			$('#second').css('marginLeft', '-=120em')
 			$('#second').show();
 			$('#second').animate({
 				marginLeft: '0em'
 			}, 1000);
 		});
 	});
+
+
+
 	$('#ctBack').on('click', function(){
 		$('#second').animate({
 			marginLeft: '-=120em'
@@ -19,14 +22,16 @@
 			$('#second').hide()
 			$('#first').show()
 			$('#first').animate({
-				marginLeft: '-=120em'
+				marginLeft: '0em'
 			}, 1000);
 		});
-	});
+	});		
+	
+
 
 	$('#example').on('click', function(){
 		$('#first').animate({
-			marginTop: '-=62em'
+			marginTop: '-62em'
 		}, 1000, function(){
 			$('#first').hide()
 			$('#third').css('marginTop', '62em')
@@ -39,19 +44,19 @@
 
 	$('#spBack').on('click', function(){
 		$('#third').animate({
-			marginTop: '+=62em'
+			marginTop: '62em'
 		}, 1000, function(){
 			$('#third').hide()
 			$('#first').show()
 			$('#first').animate({
-				marginTop: '+=62em'
+				marginTop: '0em'
 			}, 1000);
 		});
 	});
 
 	$('#portfolio').on('click', function(){
 		$('#first').animate({
-			marginLeft: '-=120em'
+			marginLeft: '-120em'
 		}, 1000, function(){
 			$('#first').hide()
 			$('#fourth').css('marginLeft', '120em')
@@ -64,12 +69,12 @@
 
 	$('#pfBack').on('click', function(){
 		$('#fourth').animate({
-			marginLeft: '+=120em'
+			marginLeft: '120em'
 		}, 1000, function(){
 			$('#fourth').hide()
 			$('#first').show()
 			$('#first').animate({
-				marginLeft: '+=120em'
+				marginLeft: '0em'
 			}, 1000);
 		});
 	});
